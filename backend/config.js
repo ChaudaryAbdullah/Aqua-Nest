36a934d5f36da7068118103d59892fb479f937bd
ChaudaryAbdullah <abdullahshakir226@gmail.com>
2025-06-21 16:47:30 +0500

    sign up page improvements

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const PORT = 5556 || 5555;
 const DB_URL =
   "mongodb+srv://abdullahshakir226:TRKs4wT33pHmpyII@cluster0.r0uvzgx.mongodb.net/";
-const FRONTEND_URL = "https://aqua-nest-chi.vercel.app/";
+const FRONTEND_URL = "https://aqua-nest-chi.vercel.app";
 export { PORT, DB_URL, FRONTEND_URL };
