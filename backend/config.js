134d5d939cd0fcc8e09fc65092aaecb0b0431868
ChaudaryAbdullah <abdullahshakir226@gmail.com>
2025-06-21 16:36:15 +0500

    deployment links added

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,5 @@
 const PORT = 5556 || 5555;
-const DB_URL = "mongodb://localhost:27017/AquaNestDB";
-const FRONTEND_URL = "http://localhost:5173";
+const DB_URL =
+  "mongodb+srv://abdullahshakir226:TRKs4wT33pHmpyII@cluster0.r0uvzgx.mongodb.net/";
+const FRONTEND_URL = "https://aqua-nest-chi.vercel.app/";
 export { PORT, DB_URL, FRONTEND_URL };
